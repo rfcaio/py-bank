c51e7d717405f7d9d6504a05372ca3bb579c1de9
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-24 14:29:51 -0300

    pyoo-aula-03

--- account.py
+++ account.py
@@ -5,3 +5,15 @@ class Account:
         self.owner = owner
         self.balance = balance
         self.limit = limit
+
+
+    def deposit(self, amount):
+        self.balance += amount
+
+
+    def print_balance(self):
+        return 'Your account balance is $ {}.'.format(self.balance)
+
+
+    def withdraw(self, amount):
+        self.balance -= amount
--- test_account.py
+++ test_account.py
@@ -4,15 +4,19 @@ from account import Account
 
 
 class TestAccount(TestCase):
-    def test_account_should_initialize_an_account_properly(self):
-        id = 477
-        owner = 'John Connor'
-        balance = 100.0
-        limit = 1000.0
+    def setUp(self):
+        self.account = Account(477, 'John Connor', 100.0, 1000.0)
 
-        a = Account(id, owner, balance, limit)
 
-        self.assertEqual(a.id, id)
-        self.assertEqual(a.owner, owner)
-        self.assertEqual(a.balance, balance)
-        self.assertEqual(a.limit, limit)
+    def test_deposit_should_increase_the_account_balance(self):
+        self.account.deposit(150.0)
+        self.assertEqual(self.account.balance, 250.0)
+
+
+    def test_print_balance_should_print_a_message_with_the_owner_balance(self):
+        self.assertEqual(self.account.print_balance(), 'Your account balance is $ 100.0.')
+
+
+    def test_withdraw_should_decrease_the_account_balance(self):
+        self.account.withdraw(50.0)
+        self.assertEqual(self.account.balance, 50.0)
