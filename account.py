e27258a6c774725998d28bb8be189147a4111b25
Caio Rodrigues <rfcaio90@gmail.com>
2019-07-25 13:34:01 -0300

    pyoo-aula-04

--- account.py
+++ account.py
@@ -1,19 +1,24 @@
 
 class Account:
     def __init__(self, id, owner, balance, limit):
-        self.id = id
-        self.owner = owner
-        self.balance = balance
-        self.limit = limit
+        self.__id = id
+        self.__owner = owner
+        self.__balance = balance
+        self.__limit = limit
 
 
     def deposit(self, amount):
-        self.balance += amount
+        self.__balance += amount
 
 
     def print_balance(self):
-        return 'Your account balance is $ {}.'.format(self.balance)
+        return 'Your account balance is $ {}.'.format(self.__balance)
+
+
+    def transfer(self, account, amount):
+        self.withdraw(amount)
+        account.deposit(amount)
 
 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
