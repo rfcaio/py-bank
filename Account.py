7ea2130d1a910a3d0cbcf23e44f24a7568c5eaa2
Caio Rodrigues <rfcaio90@gmail.com>
2020-05-18 19:46:25 -0300

    change project structure

--- Account.py
+++ Account.py
@@ -1,4 +1,3 @@
-
 class Account:
     def __init__(self, id, owner, balance, limit):
         self.__id = id
@@ -6,19 +5,15 @@ class Account:
         self.__balance = balance
         self.__limit = limit
 
-
     def deposit(self, amount):
         self.__balance += amount
-
 
     def print_balance(self):
         return 'Your account balance is $ {}.'.format(self.__balance)
 
-
     def transfer(self, account, amount):
         self.withdraw(amount)
         account.deposit(amount)
-
 
     def withdraw(self, amount):
         if (self.__can_withdraw(amount)):
@@ -26,25 +21,20 @@ class Account:
         else:
             raise ValueError('There is not enough balance.')
 
-
     def __can_withdraw(self, amount):
         return amount <= self.__balance + self.__limit
-
 
     @property
     def balance(self):
         return self.__balance
 
-
     @property
     def limit(self):
         return self.__limit
 
-
     @limit.setter
     def limit(self, limit):
         self.__limit = limit
-
 
     @property
     def owner(self):
