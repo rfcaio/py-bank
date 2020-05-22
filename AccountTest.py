7df3ecb1749fec53cb671b7ce00c43e9ef502433
Caio Rodrigues <rfcaio90@gmail.com>
2020-05-22 19:35:40 -0300

    use float numbers in test cases

--- AccountTest.py
+++ AccountTest.py
@@ -5,59 +5,59 @@ from Account import Account
 
 class AccountTest(TestCase):
     def setUp(self):
-        self.account = Account(100)
+        self.account = Account(100.0)
 
     def test_init(self):
         with self.assertRaises(ValueError):
-            Account(-100)
+            Account(-100.0)
 
         account = Account()
-        self.assertEqual(account.balance, 0)
+        self.assertEqual(account.balance, 0.0)
 
     def test_balance(self):
         with self.assertRaises(AttributeError):
-            self.account.balance = 10000
+            self.account.balance = 10000.0
 
     def test_deposit(self):
         with self.assertRaises(ValueError):
-            self.account.deposit(-100)
+            self.account.deposit(-100.0)
 
         with self.assertRaises(ValueError):
-            self.account.deposit(0)
+            self.account.deposit(0.0)
 
-        self.account.deposit(100)
-        self.assertEqual(self.account.balance, 200)
+        self.account.deposit(100.0)
+        self.assertEqual(self.account.balance, 200.0)
 
     def test_transfer(self):
-        destination = Account(50)
+        destination = Account(50.0)
 
         with self.assertRaises(TypeError):
-            self.account.transfer(50, {'balance': 50})
+            self.account.transfer(50.0, {'balance': 50.0})
 
         with self.assertRaises(ValueError):
-            self.account.transfer(-50, destination)
+            self.account.transfer(-50.0, destination)
 
         with self.assertRaises(ValueError):
-            self.account.transfer(0, destination)
+            self.account.transfer(0.0, destination)
 
         # when trying to transfer an amount greater than the balance
         with self.assertRaises(ValueError):
-            self.account.transfer(150, destination)
-            self.assertEqual(destination.balance, 50)
+            self.account.transfer(150.0, destination)
+            self.assertEqual(destination.balance, 50.0)
 
-        self.account.transfer(50, destination)
-        self.assertEqual(self.account.balance, 50)
-        self.assertEqual(destination.balance, 100)
+        self.account.transfer(50.0, destination)
+        self.assertEqual(self.account.balance, 50.0)
+        self.assertEqual(destination.balance, 100.0)
 
     def test_withdraw(self):
         with self.assertRaises(ValueError):
-            self.account.withdraw(-100)
+            self.account.withdraw(-100.0)
 
         with self.assertRaises(ValueError):
-            self.account.withdraw(0)
+            self.account.withdraw(0.0)
 
         with self.assertRaises(ValueError):
-            self.account.withdraw(500)
+            self.account.withdraw(500.0)
 
-        self.account.withdraw(50)
-        self.assertEqual(self.account.balance, 50)
+        self.account.withdraw(50.0)
+        self.assertEqual(self.account.balance, 50.0)
