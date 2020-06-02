b60f887016099579723b7e20beb5b0cb018481fe
Caio Rodrigues <rfcaio90@gmail.com>
2020-06-01 21:16:16 -0300

    bind a cpf to a user

--- User.py
+++ User.py
@@ -1,9 +1,21 @@
+from Cpf import Cpf
+
+
 class User:
-    def __init__(self, name):
+    def __init__(self, cpf, name):
         if not isinstance(name, str):
             raise TypeError('%s is not a valid name.' % name)
 
+        try:
+            self.__cpf = Cpf(cpf)
+        except Exception:
+            raise
+
         self.__name = name
+
+    @property
+    def cpf(self):
+        return self.__cpf.format()
 
     @property
     def name(self):
--- UserTest.py
+++ UserTest.py
@@ -2,14 +2,25 @@ from unittest import TestCase
 
 from User import User
 
+from exceptions import InvalidCpfError
+
 
 class UserTest(TestCase):
     def setUp(self):
-        self.user = User('John Doe')
+        self.user = User('63204728048', 'John Doe')
 
     def test_init(self):
+        with self.assertRaises(InvalidCpfError):
+            User('63204728049', 'John Doe')
+
         with self.assertRaises(TypeError):
-            User(1)
+            User('63204728048', 1)
+
+    def test_cpf(self):
+        with self.assertRaises(AttributeError):
+            self.user.cpf = '18442699066'
+
+        self.assertEqual(self.user.cpf, '632.047.280-48')
 
     def test_name(self):
         self.assertEqual(self.user.name, 'John Doe')
